77efa26ea515d3bc4d490a9a125ef5049c32cd27
ouchxp <ouchxp.me@gmail.com>
2014-05-04 04:15:02 +1200

    move networking code

--- NewFlaxAndroidLibrary/src/flax/utils/FlaxUtil.java
+++ NewFlaxAndroidLibrary/src/flax/utils/FlaxUtil.java
@@ -2,10 +2,12 @@ package flax.utils;
 
 import static flax.utils.GlobalConstants.*;
 import android.app.Application;
+import android.content.Context;
 import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.preference.PreferenceManager;
 import flax.core.FlaxApplication;
-import flax.network.NetworkConnection;
 
 public class FlaxUtil {
 	/**
@@ -14,21 +16,21 @@ public class FlaxUtil {
 	public static String getServerPath() {
 		return SpHelper.getString(SERVER_PATH_KEY, DEFAULT_SERVER_PATH);
 	}
-	
+
 	/**
 	 * setServerPath method set server path to shared pref
 	 */
 	public static void setServerPath(String serverPath) {
 		SpHelper.putSingleString(SERVER_PATH_KEY, serverPath);
 	}
-	
+
 	/**
 	 * getServerPath method Get server path from shared pref
 	 */
 	public static String getNetworkSetting() {
 		return SpHelper.getString(SERVER_PATH_KEY, DEFAULT_NETWORK_SETTING);
 	}
-	
+
 	/**
 	 * setServerPath method set server path to shared pref
 	 */
@@ -40,8 +42,26 @@ public class FlaxUtil {
 	 * isConnected method Gets the connection status.
 	 */
 	public static boolean isConnected() {
-		// get connection status
-		return new NetworkConnection(getApplication()).checkNetworkConnection();
+
+		boolean connect;
+		// Load user preferences
+		String networkPref = getNetworkSetting();
+
+		// Check the connection status of the device
+		ConnectivityManager conMan = (ConnectivityManager) getApplication().getSystemService(
+				Context.CONNECTIVITY_SERVICE);
+		NetworkInfo netInf = conMan.getActiveNetworkInfo();
+
+		// Set 'connect = true' if preferences and network status match
+		if (NETWORK_SETTING_WIFI.equals(networkPref) && netInf != null
+				&& netInf.getType() == ConnectivityManager.TYPE_WIFI) {
+			connect = true;
+		} else if (NETWORK_SETTING_ANY.equals(networkPref) && netInf != null) {
+			connect = true;
+		} else {
+			connect = false;
+		}
+		return connect;
 	}
 
 	/**
